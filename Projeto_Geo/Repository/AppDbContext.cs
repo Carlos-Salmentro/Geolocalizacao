6fc79cf32e66553f5cb8432dc47a739e2504db52
Carlos-Salmentro <carlos.salmentro@gmail.com>
2025-03-19 14:11:34 -0300

    first commit

---
+++ Projeto_Geo/Repository/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Projeto_Geo.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        
+    }
+}
